e0dfe7b835294282b8e77ff933ab735f42ec60f2
김혜원 <gpdnjs8151@naver.com>
2017-02-20 21:20:55 +0900

    project Euler no.6

---
+++ 6_projecteuler.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//var sum = 0
+//for i in 1...99 {
+//    for j in i+1...100 {
+//        sum = sum + i * j
+//    }
+//}
+//sum * 2
