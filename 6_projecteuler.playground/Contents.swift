42a7a0bb3b1691c93c54503551f1d552d3f79f95
김혜원 <gpdnjs8151@naver.com>
2017-02-20 21:24:22 +0900

    project Euler no.6

--- 6_projecteuler.playground/Contents.swift
+++ 6_projecteuler.playground/Contents.swift
@@ -4,10 +4,10 @@ import UIKit
 
 var str = "Hello, playground"
 
-//var sum = 0
-//for i in 1...99 {
-//    for j in i+1...100 {
-//        sum = sum + i * j
-//    }
-//}
-//sum * 2
+var sum = 0
+for i in 1...99 {
+    for j in i+1...100 {
+        sum = sum + i * j
+    }
+}
+sum * 2
