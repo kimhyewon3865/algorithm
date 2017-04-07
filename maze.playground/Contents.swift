cdb8ff66beadba09f685635884cd750501bb9ec7
김혜원 <gpdnjs8151@naver.com>
2017-04-07 16:13:13 +0900

    미로찾기

---
+++ maze.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let N = 8 //목적지
+var maze = [[0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 1, 1, 0, 1, 1, 0, 1],
+            [0, 0, 0, 1, 0, 0, 0, 1],
+            [0, 1, 0, 0, 1, 1, 0, 0],
+            [0, 1, 1, 1, 0, 0, 1, 1],
+            [0, 1, 0, 0, 0, 1, 0, 1],
+            [0, 0, 0, 1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0, 1, 0, 0]]
+
+let pathwayColour = 0 //white
+let wallColour = 1 //blue
+let blockedColour = 2 //red
+let pathColour = 3 //green
+
+func findMazePath(x: Int, y: Int) -> Bool {
+    if x < 0 || y < 0 || x >= N || y >= N {
+        return false
+    } else if maze[x][y] != pathwayColour {
+        return false
+    } else if x == N - 1 && y == N - 1 {
+        maze[x][y] = pathColour
+        return true
+    } else {
+        maze[x][y] = pathColour
+        if findMazePath(x: x - 1, y: y) || findMazePath(x: x, y: y + 1) || findMazePath(x: x + 1, y: y) || findMazePath(x: x, y: y - 1) {
+            return true
+        }
+        maze[x][y] = blockedColour
+        return false
+    }
+}
+
+func printMaze() {
+    for i in 0..<8 {
+        for j in 0..<8 {
+            if j != 7 {
+                print(maze[i][j], separator: "", terminator: "")
+            } else {
+                print("\n")
+            }
+        }
+    }
+}
+
+//printMaze()
+//findMazePath(x: 0, y: 0)
+//printMaze()
+print("helo")
+
+
